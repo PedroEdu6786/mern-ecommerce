e49f1ad5553a8606669b3bf0a314d151f1c988ba
PedroEdu6786 <pedrcg835@gmail.com>
2020-10-05 12:17:12 -0500

    feat(products): dynamic products on homescreen

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -4,9 +4,13 @@ import { Container, Row, Col } from 'react-bootstrap'
 const Footer = () => {
     return (
         <footer>
-            <Row>
-                <Col className="text-center py-3">Copyright &copy; ProShop</Col>
-            </Row>
+            <Container>
+                <Row>
+                    <Col className="text-center py-3">
+                        Copyright &copy; ProShop
+                    </Col>
+                </Row>
+            </Container>
         </footer>
     )
 }
