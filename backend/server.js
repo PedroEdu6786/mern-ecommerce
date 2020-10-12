0e25a0a2eac25e39a450c26cebcc0b0f353ebcb1
PedroEdu6786 <pedrcg835@gmail.com>
2020-10-12 12:17:49 -0500

    feat(back): mongoose setup

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,11 @@
 const express = require('express')
+const dotenv = require('dotenv')
 const products = require('./data/products')
+const connectDB = require('./config/db.js')
+
+dotenv.config()
+
+connectDB()
 
 const app = express()
 
@@ -16,4 +22,9 @@ app.get('/api/products/:id', (req, res) => {
     res.json(product)
 })
 
-app.listen(5000, console.log('Server running on port 5000'))
+const PORT = process.env.PORT || 5000
+
+app.listen(
+    PORT,
+    console.log(`Server running in ${process.env.NODE_ENV} on port ${5000}`)
+)
