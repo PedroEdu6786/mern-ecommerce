47b50ca5731f3529628c0b9a304a34a4d03a0ad9
PedroEdu6786 <pedrcg835@gmail.com>
2020-10-17 09:45:25 -0500

    feat(front): cartActions redux

--- frontend/src/actions/cartActions.js
+++ frontend/src/actions/cartActions.js
@@ -6,12 +6,14 @@ export const addToCart = (id, qty) => async (dispatch, getState) => {
 
     dispatch({
         type: CART_ADD_ITEM,
-        payload: data._id,
-        name: data.name,
-        image: data.image,
-        price: data.price,
-        countInStock: data.countInStock,
-        qty,
+        payload: {
+            product: data._id,
+            name: data.name,
+            image: data.image,
+            price: data.price,
+            countInStock: data.countInStock,
+            qty,
+        },
     })
 
     localStorage.setItem('cartItems', JSON.stringify(getState().cart.cartItems))
