4cc38bc6039a7c9bd7c987c50bf692cd58d14749
PedroEdu6786 <pedrcg835@gmail.com>
2020-10-18 11:25:07 -0500

    feat(back): user registration, password hashing

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -24,6 +24,35 @@ const authUser = asyncHandler(async (req, res) => {
     }
 })
 
+// @desc Register new user
+// @route POST /api/users/
+// @access Public
+const registerUser = asyncHandler(async (req, res) => {
+    const { name, email, password } = req.body
+
+    const userExists = await User.findOne({ email })
+
+    if (userExists) {
+        res.status(400)
+        throw new Error('User already exists')
+    }
+
+    const user = await User.create({ name, email, password })
+
+    if (user) {
+        res.status(201).json({
+            _id: user._id,
+            user: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+            token: generateToken(user._id),
+        })
+    } else {
+        res.status(400)
+        throw new Error('Invalid user data')
+    }
+})
+
 // @desc GET user profile
 // @route POST /api/users/profile
 // @access Private
@@ -44,4 +73,4 @@ const getUserProfile = asyncHandler(async (req, res) => {
     }
 })
 
-module.exports = { authUser, getUserProfile }
+module.exports = { authUser, registerUser, getUserProfile }
