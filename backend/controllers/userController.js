182641e13d03a6e7027c35258915e82b84ded4a0
PedroEdu6786 <pedrcg835@gmail.com>
2020-10-19 10:24:51 -0500

    feat(back): update profile route and controller

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -60,7 +60,6 @@ const getUserProfile = asyncHandler(async (req, res) => {
     const user = await User.findById(req.user._id)
 
     if (user) {
-        console.log(user._id)
         res.json({
             _id: user._id,
             user: user.name,
@@ -73,4 +72,32 @@ const getUserProfile = asyncHandler(async (req, res) => {
     }
 })
 
-module.exports = { authUser, registerUser, getUserProfile }
+// @desc UPDATE user profile
+// @route PUT /api/users/profile
+// @access Private
+const updateUserProfile = asyncHandler(async (req, res) => {
+    const user = await User.findById(req.user._id)
+
+    if (user) {
+        user.name = req.body.name || user.name
+        user.email = req.body.email || user.email
+        if (req.body.password) {
+            user.password = req.body.password
+        }
+
+        const updatedUser = await user.save()
+
+        res.json({
+            _id: updatedUser._id,
+            name: updatedUser.name,
+            email: updatedUser.email,
+            isAdmin: updatedUser.isAdmin,
+            token: generateToken(user._id),
+        })
+    } else {
+        res.status(404)
+        throw new Error('User not found')
+    }
+})
+
+module.exports = { authUser, registerUser, getUserProfile, updateUserProfile }
