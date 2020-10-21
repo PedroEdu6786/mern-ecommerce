d2ac7741e2b50892cd61cfc628ba9b994ace2d88
PedroEdu6786 <pedrcg835@gmail.com>
2020-10-21 11:09:35 -0500

    feat(front): Order screen, order details reducer actions and constants

--- frontend/src/actions/orderActions.js
+++ frontend/src/actions/orderActions.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-import { ORDER_CREATE } from '../constants/orderConstants'
+import { ORDER_CREATE, ORDER_DETAILS } from '../constants/orderConstants'
 
 export const createOrder = (order) => async (dispatch, getState) => {
     try {
@@ -25,6 +25,37 @@ export const createOrder = (order) => async (dispatch, getState) => {
     } catch (err) {
         dispatch({
             type: ORDER_CREATE._FAIL,
+            payload:
+                err.response && err.response.data.message
+                    ? err.response.data.message
+                    : err.message,
+        })
+    }
+}
+
+export const getOrderDetails = (id) => async (dispatch, getState) => {
+    try {
+        dispatch({ type: ORDER_DETAILS._REQUEST })
+
+        const {
+            userLogin: { userInfo },
+        } = getState()
+
+        const config = {
+            headers: {
+                Authorization: `Bearer ${userInfo.token}`,
+            },
+        }
+
+        const { data } = await axios.get(`/api/orders/${id}`, config)
+
+        dispatch({
+            type: ORDER_DETAILS._SUCCESS,
+            payload: data,
+        })
+    } catch (err) {
+        dispatch({
+            type: ORDER_DETAILS._FAIL,
             payload:
                 err.response && err.response.data.message
                     ? err.response.data.message
--- frontend/src/reducers/orderReducers.js
+++ frontend/src/reducers/orderReducers.js
@@ -1,4 +1,4 @@
-import { ORDER_CREATE } from '../constants/orderConstants'
+import { ORDER_CREATE, ORDER_DETAILS } from '../constants/orderConstants'
 
 export const orderCreateReducer = (state = {}, action) => {
     switch (action.type) {
@@ -9,6 +9,25 @@ export const orderCreateReducer = (state = {}, action) => {
             return { loading: false, success: true, order: action.payload }
 
         case ORDER_CREATE._FAIL:
+            return { loading: false, error: action.payload }
+
+        default:
+            return state
+    }
+}
+
+export const orderDetailsReducer = (
+    state = { loading: true, orderItems: [], shippingAddress: {} },
+    action
+) => {
+    switch (action.type) {
+        case ORDER_DETAILS._REQUEST:
+            return { ...state, loading: true }
+
+        case ORDER_DETAILS._SUCCESS:
+            return { loading: false, order: action.payload }
+
+        case ORDER_DETAILS._FAIL:
             return { loading: false, error: action.payload }
 
         default:
