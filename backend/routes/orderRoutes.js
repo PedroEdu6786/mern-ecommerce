47ce96a2f62cb68786cfb8ea61b30a4fa49ef41e
PedroEdu6786 <pedrcg835@gmail.com>
2020-10-23 10:17:48 -0500

    feat(back): order route myorders get orders from logged in user

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -4,10 +4,12 @@ const {
     addOrderItems,
     getOrderById,
     updateOrderToPaid,
+    getMyOrders,
 } = require('../controllers/orderController')
 const protect = require('../middleware/authMiddleware')
 
 router.route('/').post(protect, addOrderItems)
+router.route('/myorders').get(protect, getMyOrders)
 router.route('/:id').get(protect, getOrderById)
 router.route('/:id/pay').put(protect, updateOrderToPaid)
 
