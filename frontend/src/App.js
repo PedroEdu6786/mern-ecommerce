8d0666eb6d668078a84b362ad91321f069af0434
PedroEdu6786 <pedrcg835@gmail.com>
2020-10-28 10:53:45 -0600

    feat(front): Product edit screen, action/reducer, edit on productlist screen implemented

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -16,6 +16,7 @@ import OrderScreen from './screens/OrderScreen'
 import UserListScreen from './screens/UserListScreen'
 import UserEditScreen from './screens/UserEditScreen'
 import ProductListScreen from './screens/ProductListScreen'
+import ProductEditScreen from './screens/ProductEditScreen'
 
 const App = () => {
     return (
@@ -38,12 +39,16 @@ const App = () => {
                     <Route path="/cart/:id?" component={CartScreen} />
                     <Route path="/admin/userlist" component={UserListScreen} />
                     <Route
+                        path="/admin/user/:id/edit"
+                        component={UserEditScreen}
+                    />
+                    <Route
                         path="/admin/productlist"
                         component={ProductListScreen}
                     />
                     <Route
-                        path="/admin/user/:id/edit"
-                        component={UserEditScreen}
+                        path="/admin/product/:id/edit"
+                        component={ProductEditScreen}
                     />
                     <Route path="/" component={HomeScreen} exact />
                 </Container>
