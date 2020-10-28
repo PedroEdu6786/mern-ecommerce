05959799af1539689e8b103fa18e41a9d488794b
PedroEdu6786 <pedrcg835@gmail.com>
2020-10-28 09:50:17 -0600

    feat(back): delete product admin only route

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -3,10 +3,12 @@ const router = express.Router()
 const {
     getProducts,
     getProductById,
+    deleteProduct,
 } = require('../controllers/productController')
+const { protect, admin } = require('../middleware/authMiddleware')
 
 router.route('/').get(getProducts)
 
-router.route('/:id').get(getProductById)
+router.route('/:id').get(getProductById).delete(protect, admin, deleteProduct)
 
 module.exports = router
