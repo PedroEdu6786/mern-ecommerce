dee64867c5247cd9eeb9579d68646567a11708c9
PedroEdu6786 <pedrcg835@gmail.com>
2020-10-28 10:20:51 -0600

    feat(back): create product and update product routes added

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -4,11 +4,17 @@ const {
     getProducts,
     getProductById,
     deleteProduct,
+    createProduct,
+    updateProduct,
 } = require('../controllers/productController')
 const { protect, admin } = require('../middleware/authMiddleware')
 
-router.route('/').get(getProducts)
+router.route('/').get(getProducts).post(protect, admin, createProduct)
 
-router.route('/:id').get(getProductById).delete(protect, admin, deleteProduct)
+router
+    .route('/:id')
+    .get(getProductById)
+    .delete(protect, admin, deleteProduct)
+    .put(protect, admin, updateProduct)
 
 module.exports = router
