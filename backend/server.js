50fe038d321f77df984619dec3abc2f8d4159c0f
PedroEdu6786 <pedrcg835@gmail.com>
2020-11-17 11:49:58 -0600

    feat(back): upload file routes

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,13 @@
+const path = require('path')
 const express = require('express')
 const dotenv = require('dotenv')
+const { notFound, errorHandler } = require('./middleware/errorMiddleware')
 const connectDB = require('./config/db')
+
 const productRoutes = require('./routes/productRoutes')
 const userRoutes = require('./routes/userRoutes')
 const orderRoutes = require('./routes/orderRoutes')
-const { notFound, errorHandler } = require('./middleware/errorMiddleware')
+const uploadRoutes = require('./routes/uploadRoutes')
 
 dotenv.config()
 
@@ -21,10 +24,13 @@ app.get('/', (req, res) => {
 app.use('/api/products', productRoutes)
 app.use('/api/users', userRoutes)
 app.use('/api/orders', orderRoutes)
+app.use('/api/upload', uploadRoutes)
 
 app.get('/api/config/paypal', (req, res) =>
     res.send(process.env.PAYPAL_CLIENT_ID)
 )
+
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 
 app.use(notFound)
 
